b11540d3b598a8c636fd11ececb851a6c44971a2
farildev <memmedovfaril@gmail.com>
2024-03-26 01:00:04 +0400

    Layouts created & axios added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
+import Content from "@/views/Content";
+import Sidebar from "@/layouts/Sidebar";
 const App = () => {
   return (
-    <div>App</div>
+    <div className="flex justify-between">
+      <Sidebar />
+      <Content />
+    </div>
   )
 }
 
---
+++ src/layouts/Sidebar.jsx
@@ -0,0 +1,9 @@
+const Sidebar = () => {
+  return (
+    <aside className="w-[23%] fixed top-0 left-0 h-screen p-8 border-r border-gray-200/10">
+        <div className="text-2xl font-semibold"><span className="text-yellow-500">Ad</span>vanced.</div>
+    </aside>
+  )
+}
+
+export default Sidebar
